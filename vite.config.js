4289a0f49d7eb8623a38313efe5e6586470bbff4
ganatrap-eng <ganatra.p@gmail.com>
2026-07-11 00:29:53 +0300

    Configure Vite with alias and custom build options

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      xlsx: "xlsx/dist/xlsx.full.min.js",
+    },
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE" || warning.message.includes("externalized")) return;
+        warn(warning);
+      },
+    },
+  },
 });
